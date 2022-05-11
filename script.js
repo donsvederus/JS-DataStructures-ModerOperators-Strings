88ea71735c57f6132cffa123fac02935be89a789
Don Svederus <svederus@gmail.com>
2022-05-10 22:05:20 -0600

    0.103 Destructuring Arrays

--- script.js
+++ script.js
@@ -26,4 +26,48 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// this is destructuring the array from the right side
+// the original array is not destroyed
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+// you can skip elements in array using ,
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// without destructuring switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+// with destructuring switching variables
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receiving 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// an array with an array is ok
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default Values
+const [p = 1, q = 1, r = 1] = [8];
+console.log(p, q, r);
