2b7b04d7e7d829e7d12989b673f2c84ce0320b39
Don Svederus <svederus@gmail.com>
2022-05-19 22:44:05 -0600

    0.108 commented out

--- script.js
+++ script.js
@@ -54,13 +54,13 @@ const restaurant = {
   },
 };
 
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log(guests);
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
 
-// Nullish: null and undefined (NOT 0 or '')
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+// // Nullish: null and undefined (NOT 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
 
 // console.log("------ OR ------");
 // // Use and return ANY truthy data, short-circuiting 
