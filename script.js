448edb37d2749e6858d43ea24ab675f341ed2aef
Don Svederus <svederus@gmail.com>
2022-05-19 22:42:49 -0600

    0.108 The Nullish Coalescing

--- script.js
+++ script.js
@@ -54,6 +54,14 @@ const restaurant = {
   },
 };
 
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
 // console.log("------ OR ------");
 // // Use and return ANY truthy data, short-circuiting 
 // console.log(3 || 'Svederus'); // 3
