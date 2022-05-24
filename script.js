a56276ba41b5500fdb9da6540008da2de3f348f2
Don Svederus <svederus@gmail.com>
2022-05-24 01:19:22 -0600

    0.111 Looping Arrays The for of Loop

--- script.js
+++ script.js
@@ -54,7 +54,15 @@ const restaurant = {
   },
 };
 
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()){
+  console.log(`${i + 1}: ${el}`);
+}
+
+console.log(...menu.entries());
 
 // const game = {
 //   team1: 'Bayern Munich',
